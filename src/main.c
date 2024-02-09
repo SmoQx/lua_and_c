667796b0579ed480214790b59d7cb2b88639806e
SmoQx <smoqxpl@gmail.com>
2024-02-09 13:53:48 +0100

    09-02-2024

--- src/main.c
+++ src/main.c
@@ -44,18 +44,47 @@ void lua_example_stack(void){
 
 void lua_example_call_function(void){
     lua_State* L = luaL_newstate();
-    luaL_dofile(L, "./scripts/pythagoras.lua");
+    if (luaL_dofile(L, "./scripts/pythagoras.lua") != LUA_OK){
+        luaL_error(L, "error: %s\n", lua_tostring(L, -1));
+    }
     lua_getglobal(L, "pythagoras");
     if (lua_isfunction(L, -1)){
         lua_pushnumber(L, 3); //first arg for lua function
         lua_pushnumber(L, 4); //second arg for lua function
         const int NUM_ARGS = 2;
         const int HOW_MANY_RETURNS = 1;
+        if (lua_pcall(L, NUM_ARGS, HOW_MANY_RETURNS, 0) != LUA_OK){
+            // TODO: error
+        }else {
+            lua_Number pythaforas_result = lua_tonumber(L, -1);
+
+            printf("the pythagoras of 3 and 4 is = %f\n", (float) pythaforas_result);
+        }
+    }
+    lua_close(L);
+}
+
+int native_pythagoras(lua_State* L){
+    lua_Number b = lua_tonumber(L, -1);
+    lua_Number a = lua_tonumber(L, -2);
+    float result = (float)a*a + (float)b*b;
+    return result;
+}
+
+void lua_example_call_c_function(void){
+    lua_State* L = luaL_newstate();
+    lua_pushcfunction(L, native_pythagoras);
+    lua_setglobal(L, "native_pythagoras");
+    luaL_dofile(L, "./scripts/pythagoras-native.lua");
+    lua_setglobal(L, "pythagoras");
+    if (lua_isfunction(L, -1)) {
+        lua_pushnumber(L, 3);
+        lua_pushnumber(L, 4);
+        const int NUM_ARGS = 2;
+        const int HOW_MANY_RETURNS = 1;
         lua_pcall(L, NUM_ARGS, HOW_MANY_RETURNS, 0);
-
-        lua_Number pythaforas_result = lua_tonumber(L, -1);
-
-        printf("the pythagoras of 3 and 4 is = %f\n", (float) pythaforas_result);
+        lua_Number = result = lua_tonumber(L, -1);
+        printf("Native pythagpras (3,4) = %f\n", (float)result);
     }
     lua_close(L);
 }
@@ -64,7 +93,8 @@ int main(int argc, char *argv[]){
 //    lua_example_dotfile();
 //    lua_example_getvar();
 //    lua_example_stack();
-    lua_example_call_function();
+//    lua_example_call_function();
+    lua_example_call_c_function();
     printf("hello");
     return 0;
 }

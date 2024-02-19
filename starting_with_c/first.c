700ae66583c20ece4bc372d531311c49e3153138
SmoQx <smoqxpl@gmail.com>
2024-02-19 13:37:48 +0100

    playing with c

--- starting_with_c/first.c
+++ starting_with_c/first.c
@@ -9,9 +9,36 @@ int a(int number){
     return 1;
 };
 
+int to_binary(int provided_number){
+    int holding_number = provided_number;
+    char binary_number[provided_number / 2 + 1];
+    int iterator = 0;
+    while (holding_number >= 1) {
+        binary_number[iterator] = (holding_number % 2);
+//        printf("%i", binary_number[iterator]);
+        holding_number = holding_number / 2;
+        iterator++;
+    }
+    for (int i = 0; i < sizeof(binary_number); i++) {
+        printf("%i", binary_number[i]);
+    }
+    printf("%s", binary_number);
+    return 0;
+}
+
 int main(){
-    int what_was_returned;
+    int what_was_returned, hex_number, num_for_modulo;
+    num_for_modulo = 9;
     what_was_returned = a(1);
-    printf("What was returned %i", what_was_returned);
+    scanf("%i", &hex_number);
+//    int size_of_an_array = sizeof(to_binary(hex_number));
+//    int arr[size_of_an_array];
+//    arr[] = to_binary(hex_number);
+//    for (int i = 0; i >= sizeof(to_binary(hex_number)); i++) {
+//        printf("%i", arr[i]);
+//    } 
+    to_binary(hex_number);
+    printf("\n");
+    printf("What was returned %i and the provided number in hexadecimal is : %x", what_was_returned, hex_number);
     return 0;
 };
---
+++ starting_with_c/new.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    char input[1];
+    printf("Press Enter to continue...");
+    scanf("%s", input);
+    return 0;
+}

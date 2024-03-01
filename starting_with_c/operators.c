3c5091313a8a25dd3608e469387bdb74dc4ee1bd
SmoQx <smoqxpl@gmail.com>
2024-03-01 11:40:51 +0100

    added char text vector to operators

--- starting_with_c/operators.c
+++ starting_with_c/operators.c
@@ -5,11 +5,12 @@ int main(){
     int minutes;
     double days, years;
     intmax_t asdf;
+    char text[] = "STRINNGGG";
     printf("%ld \n %d \n", asdf, INT16_MAX);
     printf("enter the number of minutess \n");
     scanf("%d", &minutes);
     days = (minutes / 60) / 24;
     years = days / 365;
     printf("For provided minutess the number of days is %f, the number of years is %f", days, years);
-
+    printf("\n %s \n", text);
 }

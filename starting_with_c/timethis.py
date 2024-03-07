ccb81c91e197905c8d11e6f1f3077f642d347390
SmoQx <smoqxpl@gmail.com>
2024-03-07 14:16:37 +0100

    testing

--- starting_with_c/timethis.py
+++ starting_with_c/timethis.py
@@ -5,14 +5,14 @@ import resource
 
 
 
-def how_long(a: list,  int_what: str):
-    time_elapsed = timeit(lambda:subprocess.run(a), number=1)
+def how_long(a: list,  int_what: str, how_many):
+    time_elapsed = timeit(lambda:subprocess.run(a, input=str(how_many)+"\n", text=True, capture_output=True), number=1)
     peak_memory_usage_kb = resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss
     print(f"time elapesed in {int_what}, is: {time_elapsed:.6f} seconds, memory used {peak_memory_usage_kb/1024} MB")
 
 
 if __name__ == "__main__":
-    how_long(a = ["./count", "10000000"], int_what = "C")
-    how_long(a= ["lua", "count.lua", "10000000"], int_what="lua")
-    how_long(a= ["python3", "count.py", "10000000"], int_what="Python")
+    how_long(a = ["./count"], int_what = "C", how_many=1000000000)
+    how_long(a= ["lua", "count.lua"], int_what="lua", how_many=10000000)
+    how_long(a= ["python3", "count.py"], int_what="Python", how_many=10000000)
 

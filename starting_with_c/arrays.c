5e4835791090758baa555079fb6d75d7703a0e2d
SmoQx <smoqxpl@gmail.com>
2024-03-08 14:08:28 +0100

    arays test

--- starting_with_c/arrays.c
+++ starting_with_c/arrays.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-   
 
-    void main()
+//int lenghtof(int array) {
+//    return sizeof(array) / sizeof(array[0]);
+//    
+//}
 
-    {
 
-        int k;
-
-        for (k = -3; k < -5; k++)
-
-            printf("Hello");
-
+int main() {
+    char tekst[] = "teskt";
+    int k[5] = {10, 12, 13};
+    int i = 2;
+    printf("%zd\n", sizeof(k) / sizeof(k[0]));
+    for (int x = 0; x < (sizeof(k) / sizeof(k[0])); x++) {
+        printf("%d,", k[x]);
     }
+    k[0] = 11;
+    printf("\n %d \n", k[0]);
+    k[3] = 40;
+    printf("%2u \n", i);
+    printf("%d\n", k[4]);
+    printf("%s", tekst);
+
+}

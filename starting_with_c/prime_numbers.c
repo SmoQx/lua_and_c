5399aebe31c30800488f777e43a60d977ec3fd4c
SmoQx <smoqxpl@gmail.com>
2024-03-11 13:34:47 +0100

    shit shit fuck fuck

--- starting_with_c/prime_numbers.c
+++ starting_with_c/prime_numbers.c
@@ -12,26 +12,30 @@ bool check_if_prime(int number, int number_to_compare){
 
 
 int main(){
-    int prime_numbers[100] = {1, 2, 3};
+    int prime_numbers[100] = {1};
+    prime_numbers[1] = 2;
+    prime_numbers[2] = 3;
     int i = 5;
     int p;
     int prime_index = 3;
-    while (true) {
-        if (!(prime_numbers[99] == 0)) {
-            break;
-        }
-        for (p = 2; p < prime_index; p++) {
-            if (check_if_prime(i, prime_numbers[p]) && i / prime_numbers[p] >= prime_numbers[p]) {
-                prime_numbers[prime_index] = i;
-                prime_index++;
-            } else {
-                printf("the number is not prime%d \n", i);
-            }
+    while (prime_numbers[99]==1) {
+        printf("%d ", i);
+        if (i > 150){
+            prime_numbers[99] = 10;
         }
         i = i + 2;
     }
+//        for (p = 1; p < prime_index; p++) {
+//            if (i % i == 0 && i % prime_numbers[p] != 0 && i / prime_numbers[p] >= prime_numbers[p]) {
+//                prime_numbers[prime_index] = i;
+//                prime_index++;
+//            } else {
+//                printf("the number is not prime%d \n", i);
+//                break;
+//            }
+//        }
 
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < 10; i++) {
         printf("prime numbers %d \n", prime_numbers[i]);
     }
 }

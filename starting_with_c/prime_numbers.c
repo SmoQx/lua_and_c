145d412e21e3ccdd0b5398612dc5260249b1c43b
SmoQx <smoqxpl@gmail.com>
2024-03-11 11:37:07 +0100

    changed the prime_numbers

--- starting_with_c/prime_numbers.c
+++ starting_with_c/prime_numbers.c
@@ -13,19 +13,25 @@ bool check_if_prime(int number, int number_to_compare){
 
 int main(){
     int prime_numbers[100] = {1, 2, 3};
-    int i;
+    int i = 5;
     int p;
     int prime_index = 3;
-    for (i = 5; i < 50; i = i + 2) {
+    while (true) {
+        if (!(prime_numbers[99] == 0)) {
+            break;
+        }
         for (p = 2; p < prime_index; p++) {
-            if (check_if_prime(i, prime_numbers[p]) ) {
+            if (check_if_prime(i, prime_numbers[p]) && i / prime_numbers[p] >= prime_numbers[p]) {
                 prime_numbers[prime_index] = i;
                 prime_index++;
+            } else {
+                printf("the number is not prime%d \n", i);
             }
         }
+        i = i + 2;
     }
 
-    for (i = 0; i < 30; i++) {
+    for (i = 0; i < 100; i++) {
         printf("prime numbers %d \n", prime_numbers[i]);
     }
 }

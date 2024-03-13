1fa43982d38be73d0dfa97cda13906b4bf46366c
SmoQx <smoqxpl@gmail.com>
2024-03-13 13:52:32 +0100

    weather program

---
+++ starting_with_c/avg_rain.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+    float rain_per_years[5][12] = {
+                                    {},
+                                    {},
+                                    {},
+                                    {},
+                                    {}
+                                    };
+
+    for (int x = 0; x < sizeof(rain_per_years) / sizeof(rain_per_years[0]); x++) {
+        for (int y = 0; y < sizeof(rain_per_years) / sizeof(rain_per_years[0]); y++) {
+            
+        }
+    }
+}

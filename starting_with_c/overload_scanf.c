b8a838ed3bc076b1ec3fb9df6b67b9c81d89eec8
SmoQx <smoqxpl@gmail.com>
2024-03-14 13:49:49 +0100

    asd

--- starting_with_c/overload_scanf.c
+++ starting_with_c/overload_scanf.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 
+int add_two_and_divide(int a, int b, int c){
+    int result;
+    result = a * b + c;
+    return result;
+}
+
+
 int main(){
+
+    printf("%d\n", add_two_and_divide(5, 5, 2));
     int input;
     scanf("%d", &input);
     int added = input + 10;

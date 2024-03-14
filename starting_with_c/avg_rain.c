8f6dedfdd0c3d4f2a97474738b1377b0bdf8ca3e
SmoQx <smoqxpl@gmail.com>
2024-03-14 08:13:58 +0100

    yes

--- starting_with_c/avg_rain.c
+++ starting_with_c/avg_rain.c
@@ -6,6 +6,8 @@
 
 
 int main(){
+    int year, month;
+    float subtotal, total;
     float rain_per_years[YEARS][MONTHS] = {
                                     {0.2, 0.3, 0.8, 1, 1.2, 4, 3.2, 5.3, 1.2, 1.4, 3.2, 4.1},
                                     {0.2, 0.3, 0.8, 1, 1.2, 4, 3.2, 5.3, 1.2, 1.4, 3.2, 4.1},
@@ -14,9 +16,15 @@ int main(){
                                     {0.2, 0.3, 0.8, 1, 1.2, 4, 3.2, 5.3, 1.2, 1.4, 3.2, 4.1}
                                     };
 
-    for (int x = 0; x < sizeof(rain_per_years) / sizeof(rain_per_years[0]); x++) {
-        for (int y = 0; y < sizeof(rain_per_years[0]) / sizeof(rain_per_years[0][0]); y++) {
-            
+    for (year = 0, total = 0; year < sizeof(rain_per_years) / sizeof(rain_per_years[0]); year++) {
+        for (month = 0, subtotal = 0; month < sizeof(rain_per_years[0]) / sizeof(rain_per_years[0][0]); month++) {
+            subtotal += rain_per_years[year][month];
         }
+        printf("%5d %15.1f\n", 2010 + year, subtotal);
+        total += subtotal;
     }
+
+    printf("\nThe yearly average in %.1f inches. \n\n", total / YEARS);
+
+    return 0;
 }

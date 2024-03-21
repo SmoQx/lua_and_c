17ad6e751dbb317927fca3c88d4364d2c7683753
SmoQx <smoqxpl@gmail.com>
2024-03-21 22:04:19 +0100

    arrays

--- starting_with_c/sort_a_string_array.c
+++ starting_with_c/sort_a_string_array.c
@@ -11,8 +11,8 @@ void print_list(int size, int lenght, char list[size][lenght]) {
 
 void sort_string_array(char sorted_string_array[3][50], char list_to_be_sorted[3][50]) {
     char temp[50]; 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 0; j <= 3 - i; j++) {
             if (strcmp(list_to_be_sorted[j], list_to_be_sorted[j+1]) > 0) {
                 strcpy(temp, list_to_be_sorted[j]);
                 strcpy(sorted_string_array[j], list_to_be_sorted[j+1]);

3ec4a6c2036fa5e85db257af5cb854a2a746d69a
SmoQx <smoqxpl@gmail.com>
2024-03-26 12:07:54 +0100

    stil playing with pointers

--- starting_with_c/pointers_array.c
+++ starting_with_c/pointers_array.c
@@ -3,16 +3,20 @@
 
 int main(void) {
     int vals[10];
-    int *ptr_vals = vals;
+    int *const ptr_vals = vals;
     int counter = 0;
-    for (int i = 0; i <= 10000; i++) {
-        if (i % 100 == 0) {
-            printf("%d\t", *(ptr_vals));
-        }
-        if (counter == 11) {
-            counter = 0;
+    printf("\n%p\n", &vals);
+    printf("\n%p\n", ptr_vals);
+    for (int i = 0; i <= 20; i ++) {
+        if (counter == 10) {
+            counter = -10;
         }
         *(ptr_vals + counter) = i;
+        printf("%p\n", ptr_vals + counter);
+        counter++;
+    }
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", vals[i]);
     }
     return 0;
 }
